73eff17c0623eab1363fe3ad7505af3d68bd5146
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-08-05 21:26:58 +0430

    for push

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,4 +9,12 @@ class Expense(models.Model) :
     date = models.DateField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    
+    def __str__(self):
+        return '%s %s %s %s' % (self.text,self.date, self.amount, "EXPENSE") 
+class Income(models.Model) :
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    def __str__(self):
+        return '%s %s %s %s' % (self.text, self.date, self.amount, "INCOME") 
